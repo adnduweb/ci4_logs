8ad12d11a93822f7115c0e702b5686f2e6062744
Loru Fabrice <fabrice@adnduweb.com>
2020-06-19 19:46:54 +0200

    Nettoyage

--- src/Models/AuditModel.php
+++ src/Models/AuditModel.php
@@ -1,35 +1,37 @@
-<?php namespace Adnduweb\Ci4_logs\Models;
+<?php
+
+namespace Adnduweb\Ci4_logs\Models;
 
 use CodeIgniter\Model;
 
 class AuditModel extends Model
 {
-	protected $table      = 'audits';
-	protected $primaryKey = 'id';
+    protected $table      = 'audits';
+    protected $primaryKey = 'id';
 
-	protected $returnType = 'object';
-	protected $useSoftDeletes = false;
+    protected $returnType = 'object';
+    protected $useSoftDeletes = false;
 
-	protected $allowedFields = ['source', 'source_id', 'user_id', 'event', 'summary', 'created_at'];
+    protected $allowedFields = ['source', 'source_id', 'user_id', 'event', 'summary', 'created_at'];
 
-	protected $useTimestamps = false;
+    protected $useTimestamps = false;
 
-	protected $validationRules    = [];
-	protected $validationMessages = [];
-	protected $skipValidation     = false;
+    protected $validationRules    = [];
+    protected $validationMessages = [];
+    protected $skipValidation     = false;
 
-	public function __construct()
+    public function __construct()
     {
         parent::__construct();
         $this->audits = $this->db->table('audits');
     }
 
-	public function getAllList(int $page, int $perpage, array $sort, array $query)
+    public function getAllList(int $page, int $perpage, array $sort, array $query)
     {
         $this->audits->select();
         $this->audits->select('created_at as date_create_at');
-        if (isset($query['generalSearch']) && !empty($query['generalSearch'])) {
-            $this->audits->where('(source LIKE "%' . $query['generalSearch'] . '%" OR event LIKE "%' . $query['generalSearch'] . '%")');
+        if (isset($query[0]) && is_array($query)) {
+            $this->audits->where('(source LIKE "%' . $query[0] . '%" OR event LIKE "%' . $query[0] . '%")');
             $this->audits->limit(0, $page);
         } else {
             $page = ($page == '1') ? '0' : (($page - 1) * $perpage);
@@ -46,9 +48,9 @@ class AuditModel extends Model
     public function getAllCount(array $sort, array $query)
     {
         $this->audits->select('id');
-        if (isset($query['generalSearch']) && !empty($query['generalSearch'])) {
-            $this->audits->where('(source LIKE "%' . $query['generalSearch'] . '%" OR event LIKE "%' . $query['generalSearch'] . '%")');
-		}
+        if (isset($query[0]) && is_array($query)) {
+            $this->audits->where('(source LIKE "%' . $query[0] . '%" OR event LIKE "%' . $query[0] . '%")');
+        }
 
         $this->audits->orderBy($sort['field'] . ' ' . $sort['sort']);
 
